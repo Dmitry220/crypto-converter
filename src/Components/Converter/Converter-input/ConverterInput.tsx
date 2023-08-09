6642790e7099fd6aefed354ea775806c573a50a5
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-08-09 18:38:41 +0300

    refactoring

---
+++ src/Components/Converter/Converter-input/ConverterInput.tsx
@@ -0,0 +1,71 @@
+import {PopupConverter} from "../popup-converter/Popup-converter";
+import React, {ChangeEvent, Dispatch, FC, useRef, useState} from "react";
+import arrowDoubleVertical from '../../assets/images/arrow-double-vertical.svg'
+import {IValutions} from "../../models/IValutions";
+import {valuations} from "../../utils/Constants";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
+import {regexInput} from "../../utils/helperFunctions";
+
+interface IConverterInput {
+    index: 1| 2,
+    value: string,
+    setValue: Dispatch<string>
+    valuation: IValutions
+    setValuation: Dispatch<IValutions>
+    setFocus: Dispatch<1|2>
+
+}
+
+export const ConverterInput: FC<IConverterInput> = ({index,setValue,value,valuation,setValuation,setFocus}) => {
+
+    const [showPopup, setShowPopup] = useState<boolean>()
+
+    const ref = useRef<HTMLDivElement>(null)
+
+    const handlerInput = (e: ChangeEvent<HTMLInputElement>) => {
+        regexInput(e)
+        setValue(e.target.value)
+    }
+
+    useOnClickOutside(ref, () => {
+        setShowPopup(false)
+    })
+
+    return (
+        <div className='converter__row converter__row_border'>
+            <input
+                className='converter__input'
+                min={0}
+                type='text'
+                maxLength={20}
+                value={value}
+                onFocus={()=>setFocus(index)}
+                onChange={handlerInput}
+            />
+            <span
+                className='converter__crypto-name'
+                onClick={()=>setShowPopup(prev=>!prev)}
+            >
+            <div className={'converter__crypto-name-title'}>
+              <img src={valuation.img} alt='' className={'converter__crypto-icon'}/>
+                {valuation.title}
+            </div>
+            <div>
+              <img src={arrowDoubleVertical} alt="" className={'converter__crypto-name-arrow'}/>
+            </div>
+          </span>
+            {showPopup && (
+                <div className='converter__popup popup-converter' ref={ref}>
+                    {valuations.map((item, index) => (
+                        <PopupConverter
+                            key={index}
+                            item={item}
+                            valuation={valuation}
+                            setValuation={setValuation}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
---
+++ src/Components/Converter/popup-converter/Popup-converter.tsx
@@ -0,0 +1,39 @@
+import React, {FC, MouseEvent} from 'react'
+import {IListValuationsName} from '../../models/IListValutionsName'
+import checkIcon from "../../assets/images/check.svg"
+import './Popup-converter.scss'
+
+export const PopupConverter: FC<IListValuationsName> = (
+    {
+        item,
+        valuation,
+        setValuation
+    }) => {
+
+    const setActiveValuation = (e: MouseEvent<HTMLDivElement>) => {
+        setValuation({
+            img: item.img,
+            title: item.title
+        })
+    }
+
+    return (
+        <div className={'popup-converter__container'} onClick={setActiveValuation}>
+            <div className={'popup-converter__check'}>
+                {valuation.title === item.title && (
+                    <img src={checkIcon} alt="check"/>
+                )}
+            </div>
+            <div className={'popup-converter__body'}>
+                <img src={item.img} alt='' className={'popup-converter__img'}/>
+                <button
+                    type='button'
+                    className={'popup-converter__button'}
+                    value={item.title}
+                >
+                    {item.title} <span>{item.alt}</span>
+                </button>
+            </div>
+        </div>
+    )
+}
