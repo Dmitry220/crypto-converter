693da29d69173dac2e15a1f4c3ee2626e5c7b082
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-08-09 13:23:14 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './App.scss';
 import btc from '../src/assets/images/btc.svg'
 import usdt from '../src/assets/images/usdt.svg'
 import eth from '../src/assets/images/eth.svg'
 import useOnClickOutside from './hooks/useOnClickOutside';
-import {PopupConverter} from './Components/popup-converter/Popup-converter';
-import {convert} from '.';
-import {IValutions} from './models/IValutions';
+import { PopupConverter } from './Components/popup-converter/Popup-converter';
+import { convert } from '.';
+import { IValutions } from './models/IValutions';
 
 function App() {
 
@@ -30,23 +30,29 @@ function App() {
 
     const ref = useRef<HTMLDivElement>(null);
 
-    const [firstValue, setFirstValue] = useState<number>(0)
-    const [secondValue, setSecondValue] = useState<number>(0)
+    const [firstValue, setFirstValue] = useState<number>(1)
+    const [secondValue, setSecondValue] = useState<number>(1)
     const [firstValuation, setFirstValuation] = useState<IValutions>(valuations[0])
     const [secondValuation, setSecondValuation] = useState<IValutions>(valuations[1])
     const [showFirstValuation, setShowFirstValuation] = useState<boolean>()
     const [showSecondValuation, setShowSecondValuation] = useState<boolean>()
-    const [isSwap, setSwap] = useState(false)
+    const [isSwap, setSwap] = useState(true)
 
 
     const handlerInputOne = (e: ChangeEvent<HTMLInputElement>) => {
-        setFirstValue(+e.target.value)
-        getSecondValuation(+e.target.value)
+        if (+e.target.value === 0) {
+            return
+        }
+        setFirstValue(+e.target.value.replace(/^0+(?!\.|$)/, ''))
+        getSecondValuation(+e.target.value.replace(/^0+(?!\.|$)/, ''))
     }
 
     const handlerInputTwo = (e: ChangeEvent<HTMLInputElement>) => {
-        setSecondValue(+e.target.value)
-        getFirstValuation(+e.target.value)
+        if (+e.target.value === 0) {
+            return
+        }
+        setSecondValue(+e.target.value.replace(/^0+(?!\.|$)/, ''))
+        getFirstValuation(+e.target.value.replace(/^0+(?!\.|$)/, ''))
     }
 
     const openFirstListValuation = () => setShowFirstValuation(true)
@@ -95,17 +101,17 @@ function App() {
             <div className="converter__body">
                 <div className="converter__row converter__row_border">
                     <input className='converter__input' min={0} type="number" value={firstValue}
-                           onChange={handlerInputOne}/>
+                        onChange={handlerInputOne} />
                     <span className='converter__crypto-name' onClick={openFirstListValuation}>
-            <div>
-              <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="8" height="14"><path
-                  d="M4 0l4 6H0l4-6zm0 14l4-6H0l4 6z"/></svg>
-            </div>
-            <div className={'converter__crypto-name-title'}>
-                <img src={firstValuation.img} alt=""/>
-                {firstValuation.title}
-            </div>
-          </span>
+                        <div>
+                            <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="8" height="14"><path
+                                d="M4 0l4 6H0l4-6zm0 14l4-6H0l4 6z" /></svg>
+                        </div>
+                        <div className={'converter__crypto-name-title'}>
+                            <img src={firstValuation.img} alt="" />
+                            {firstValuation.title}
+                        </div>
+                    </span>
                     {showFirstValuation && <div className='converter__popup popup-converter' ref={ref}>
                         {valuations.map((item, index) =>
                             <PopupConverter
@@ -120,22 +126,22 @@ function App() {
                     }
                 </div>
                 <div className="converter__row">
-                  <div className={'converter__arrow'} onClick={swap} />
+                    <div className={'converter__arrow'} onClick={swap} />
                 </div>
                 <div className="converter__row converter__row_border">
                     <input className='converter__input' min={0} type="number" maxLength={20} value={secondValue}
-                           onChange={handlerInputTwo}/>
+                        onChange={handlerInputTwo} />
                     <span className='converter__crypto-name' onClick={openSecondListValuation}>
-             <div>
-              <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="8" height="14"><path
-                  d="M4 0l4 6H0l4-6zm0 14l4-6H0l4 6z" /></svg>
-            </div>
-              <div className={'converter__crypto-name-title'}>
-               <img src={secondValuation.img} alt=""/>
-                  {secondValuation.title}
-            </div>
+                        <div>
+                            <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="8" height="14"><path
+                                d="M4 0l4 6H0l4-6zm0 14l4-6H0l4 6z" /></svg>
+                        </div>
+                        <div className={'converter__crypto-name-title'}>
+                            <img src={secondValuation.img} alt="" />
+                            {secondValuation.title}
+                        </div>
 
-          </span>
+                    </span>
                     {showSecondValuation && <div className='converter__popup popup-converter' ref={ref}>
                         {valuations.map((item, index) =>
                             <PopupConverter
@@ -150,7 +156,7 @@ function App() {
             </div>
             <footer className="converter__footer">
                 <b>{(firstValue || 0) + ' ' + firstValuation.title + ' = ' + (secondValue || 0) + ' ' + secondValuation.title}</b>
-                <br/>
+                <br />
                 Данные носят ознакомительный характер {'\t'}
                 <b>
                     {
